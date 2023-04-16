f75c5e7072ddb1b4871ed0ee112f58ca99334b8f
Chandan <cmahato2000@gmail.com>
2023-04-16 23:24:23 +0545

    working on common-bus

--- S2GC/script/index.js
+++ S2GC/script/index.js
@@ -1,12 +1,12 @@
 let scriptList = [
-  "./script/navBar/navbar.js",
-  "./script/memory/memory.js",
-  "./script/memory/slider.js",
-  "./script/memory/hover.js",
-  "./script/memory/edit.js",
-  "./script/memory/toggle.js",
-  "./script/memory/operation.js",
-  "./script/codeEditor/model.js",
+  "../script/navBar/navbar.js",
+  "../script/memory/memory.js",
+  "../script/memory/slider.js",
+  "../script/memory/hover.js",
+  "../script/memory/edit.js",
+  "../script/memory/toggle.js",
+  "../script/memory/operation.js",
+  "../script/codeEditor/model.js",
 ];
 scriptList.forEach(function (s) {
   let script = document.createElement("script"); //creating <script> element
