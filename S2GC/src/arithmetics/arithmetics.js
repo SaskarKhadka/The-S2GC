308061a8b176f90da0c864721f41d6215c294b8e
Saskar Khadka <saskar.khadka@gmail.com>
2023-05-06 21:55:43 +0545

    arithmetics update

--- S2GC/src/arithmetics/arithmetics.js
+++ S2GC/src/arithmetics/arithmetics.js
@@ -37,7 +37,9 @@ class Arithmetics {
   static add(num1, num2) {
     let sum = "";
     let carry = "0";
+    let carryNMinus1 = carry;
     for (let i = num1.length - 1; i >= 0; i--) {
+      carryNMinus1 = carry;
       if (num1[i] == num2[i] && carry == "1") {
         sum = "1" + sum;
         carry = num1[i] == "1" ? "1" : "0";
@@ -52,7 +54,12 @@ class Arithmetics {
         carry = "0";
       }
     }
-    return [sum, carry];
+    const overflow = carry == carryNMinus1 ? "0" : "1";
+    return { sum, carry, overflow };
+  }
+
+  static subtract(num1, num2) {
+    return Arithmetics.add(num1, Arithmetics.twosComplement(num2));
   }
 
   static twosComplement(num) {
@@ -61,13 +68,22 @@ class Arithmetics {
       onesComp += num[i] == "0" ? "1" : "0";
     }
     let one = Arithmetics.createStandardSize("1", onesComp.length);
-    return Arithmetics.add(onesComp, one)[0];
+    return Arithmetics.add(onesComp, one).sum;
   }
 
   static increament(num) {
     let one = Arithmetics.createStandardSize("1", num.length);
     let result = Arithmetics.add(num, one);
-    return result;
+    return result.sum;
+  }
+
+  static decreament(num) {
+    let one = Arithmetics.createStandardSize("1", num.length);
+    let twosCompOfOne = Arithmetics.twosComplement(one);
+
+    let result = Arithmetics.add(num, twosCompOfOne);
+
+    return result.sum;
   }
 
   static clear(num) {
@@ -76,5 +92,71 @@ class Arithmetics {
       result += "0";
     }
     return result;
+  }
+
+  static and(num1, num2) {
+    let result = "";
+    for (let i = 0; i < num1.length; i++) {
+      result += num1[i] == "0" || num2[i] == "0" ? "0" : "1";
+    }
+    return result;
+  }
+
+  static or(num1, num2) {
+    let result = "";
+    for (let i = 0; i < num1.length; i++) {
+      result += num1[i] == "1" || num2[i] == "1" ? "1" : "0";
+    }
+    return result;
+  }
+
+  static xor(num1, num2) {
+    let result = "";
+    for (let i = 0; i < num1.length; i++) {
+      result += num1[i] == num2[i] ? "0" : "1";
+    }
+    return result;
+  }
+
+  static nand(num1, num2) {
+    let result = "";
+    for (let i = 0; i < num1.length; i++) {
+      result += num1[i] == "0" || num2[i] == "0" ? "1" : "0";
+    }
+    return result;
+  }
+
+  static nor(num1, num2) {
+    let result = "";
+    for (let i = 0; i < num1.length; i++) {
+      result += num1[i] == "1" || num2[i] == "1" ? "0" : "1";
+    }
+    return result;
+  }
+
+  static complement(num1) {
+    let result = "";
+    for (let i = 0; i < num1.length; i++) {
+      result += num1[i] == "1" ? "0" : "1";
+    }
+    return result;
+  }
+
+  static ashl(num) {
+    let result = "";
+    for (let i = 1; i < num.length; i++) {
+      result += num[i];
+    }
+    result += "0";
+    const overflow = num[0] == num[1] ? "0" : "1";
+    return { result, carry: num[0], overflow };
+  }
+
+  static ashr(num) {
+    let result = num[0];
+    for (let i = 0; i < num.length - 1; i++) {
+      result += num[i];
+    }
+    return { result, carry: num[num.length - 1] };
   }
 }
