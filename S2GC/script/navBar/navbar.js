e3e50c035880ab2eaefce59543b1e78b233c16c3
Chandan <cmahato2000@gmail.com>
2023-05-14 03:38:52 +0545

    navbar, video, etc

--- S2GC/script/navBar/navbar.js
+++ S2GC/script/navBar/navbar.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const help = document.getElementById("open-help");
+const about = document.getElementById("about");
+const github = document.getElementById("github");
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -7,3 +10,23 @@ function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
+
+window.onscroll = function () {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+};
+
+help.addEventListener("click", function () {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+});
+
+about.addEventListener("click", function () {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+});
+
+github.addEventListener("click", function () {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+});
